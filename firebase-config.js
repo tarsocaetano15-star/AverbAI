7caa73376fa7f388afa5b1b4ac47482dc5ce0013
tarsocaetano15-star <tarsocaetano15@gmail.com>
2026-05-03 01:47:09 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,8 @@
 const firebaseConfig = {
   apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
   messagingSenderId: "SEU_ID",
   appId: "SEU_APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
-
 const messaging = firebase.messaging();
